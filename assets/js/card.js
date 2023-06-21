00530e65679368955e71f42a0eb5587fa0ab8adf
GABRIEL PRATES BITENCOURT <07001230162@ifms.edu.br>
2023-06-20 22:32:24 -0400

    tab menu

---
+++ assets/js/card.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+
+
+  const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,3 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
     }
 }
-
-const swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-       invert: false,
-    },
-    // autoHeight: true,
-    pagination: {
-       el: '.blog-slider__pagination',
-       clickable: true,
-    }
- });
